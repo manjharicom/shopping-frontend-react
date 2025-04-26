176a89bce5bdee68ea8aafc900d73d2fc1640126
John Harry <john@manjhari.com>
2025-04-26 17:43:12 +1200

    search 2

---
+++ shopping-frontend/app/ui/search/results2.tsx
@@ -0,0 +1,37 @@
+import { ProductModel } from "@/app/lib/definitions";
+
+export default async function SearchResults({products} : {products : ProductModel[]}) {
+    return (
+        <div className="inline-block min-w-full align-middle">
+              <div
+                  className="mb-2 w-full rounded-md bg-white p-4">
+                  <table>
+                    <thead>
+                      <tr>
+                      <th>
+                        Product Name
+                      </th>
+                      <th>
+                        Options
+                      </th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                    {products.map((product) => (  
+                      <tr key={product.productId}>
+                        <td>{product.name}</td>
+                        <td>
+                          <div>
+                            {product.shoppingListId ? <button>Remove</button> : null}
+                            {!product.shoppingListId ? <button>Add to List</button> : null}
+                            <button>Edit</button>
+                          </div>
+                        </td>
+                      </tr>
+                    ))}
+                    </tbody>
+                  </table>
+              </div>
+        </div>
+    );
+}
